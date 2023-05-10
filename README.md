596e16433414983f8decab908d930ce7037d60c1
Pride <xixipepperd@gmail.com>
2023-05-09 20:06:14 -0400

    project complete

--- README.md
+++ README.md
@@ -1,25 +1,110 @@
-# Cancerous Cells Classifier and Metastasis Predictor
-Running naive Bayes and support vector machine classifiers using scikit-learn on benign/malignant cancer cell data sets to predict metastasis.
+# Classification of Cancerous Cells and Cancerous Nature of Tumors of Wisconsin Patients with Breast Cancer
+Running naive Bayes and support vector machine classifiers using scikit-learn on benign/malignant cancer 
+cell data sets to predict cancerous nature of the patient. Data accessed from the
+**Breast Cancer Wisconsin (Diagnostic) Data Set**:
+https://archive.ics.uci.edu/ml/datasets/breast+cancer+wisconsin+(diagnostic).
 - **Pride Y**.
 
-> ## IN DEVELOPMENT
+## Training data
+
+We will train the data from this data set running three different classifier algorithms (**Naive Bayes**, 
+**Logistic Regression** and **Support Vector Machines**).
+
+### Data correlation
+
+We find that some *features* in this data set are highly correlated, such as worst perimeter and worst
+radius, and some little correlation, mean symmetry and mean smoothness.
+
+![heatmap correlation.png](readme%2Fheatmap%20correlation.png)
+
+### Visualization
+
+We can also visualize the separability of our trained data.
+
+![trained data scatter plot.png](readme%2Ftrained%20data%20scatter%20plot.png)
 
 ## Classifier
 
 This program rudimentally classifies cancer cell data on benignity and malignancy from scikit-learn's 
-existing breast cancer data set using Naive Bayes and Support Vector Machine classifiers. (*In development*)
+existing breast cancer data set running **Naive Bayes**, **Logistic Regression** and 
+**Support Vector Machine** classifiers.
 
 We will train the data, then run it through a confusion matrix to identify the correctness of our data
-for both classifiers.
+for all three classifiers.
 
-## Predictor
+### Naive bayes
 
-This program attempts to predict the metastatic growth of cancer cells based on the results
-provided from our classification above. (*In development*)
+![nb confusion matrix.png](readme%2Fnb%20confusion%20matrix.png)
+
+![nb effectiveness.png](readme%2Fnb%20effectiveness.png)
+
+We find that 89 patients were correctly identified to have a tumor that is malignant, 180 patients were 
+correctly identified to have benign tumors, and a 94.79% effectiveness in classification.
+
+### Logistic regression
+
+![logreg confusion matrix.png](readme%2Flogreg%20confusion%20matrix.png)
+
+![logreg effectiveness.png](readme%2Flogreg%20effectiveness.png)
+
+We find that 90 patients were correctly identified to have a tumor that is malignant, 180 patients were 
+correctly identified to have benign tumors, and a 94.74% effectiveness in classification.
+
+### Support vector machines
+
+![svm confusion matrix.png](readme%2Fsvm%20confusion%20matrix.png)
+
+![svm effectiveness.png](readme%2Fsvm%20effectiveness.png)
+
+We find that 93 patients were correctly identified to have a tumor that is malignant, 182 patients were 
+correctly identified to have benign tumors, and a 96.49% effectiveness in classification.
+
+### Conclusion
+
+We can conclude that support vector machines are most accurate in predicting the cancerous nature of
+a patient's tumor in this data set.
+
+## Scrambled data
+
+To test the algorithms in a less controlled manner, I scrambled data to see the results. Surprisingly,
+support vector machines and logistic regression algorithms provided more similar results as compared to
+naive bayes.
+
+> ### Naive Bayes
+
+![nb confusion matrix random.png](readme%2Fnb%20confusion%20matrix%20random.png)
+
+> ### Logistic regression
+
+![logreg confusion matrix random.png](readme%2Flogreg%20confusion%20matrix%20random.png)
+
+> ### Support vector machines
+
+![svm confusion matrix random.png](readme%2Fsvm%20confusion%20matrix%20random.png)
 
 ## Dependencies
 
 ### scikit-learn
 ```js
 pip install scikit-learn
+```
+
+### matplotlib
+```js
+pip install matplotlib
+```
+
+### pandas
+```js
+pip install pandas
+```
+
+### numpy
+```js
+pip install numpy
+```
+
+### seaborn
+```js
+pip install seaborn
 ```
--- classifier.py
+++ classifier.py
@@ -1,35 +1,108 @@
 from sklearn.datasets import load_breast_cancer
-
-data = load_breast_cancer()
-data.target_names
+import matplotlib.pyplot as plt
+import pandas as panda
+import numpy as np
+import seaborn as sns
 
 from sklearn.model_selection import train_test_split
-
-# Get trained/test data
-train, test, training_labels, test_labels = train_test_split(data['data'], data['target'], test_size=0.8, random_state=114)
 
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 
-# Implement Naive Bayes
-model = GaussianNB()
+data = load_breast_cancer()
 
-# Train our data using Naive Bayes
-model.fit(train, training_labels)
+# Because the data is basically everywhere and unreadable/unanalyzable, we will use pandas to generate some
+# sort of visual in the form of a table/data frame
 
-# Generate predictions
-print(confusion_matrix(test_labels, model.predict(test)))
-print(accuracy_score(test_labels, model.predict(test)))
+np_data = np.c_[data['data'], data['target']]
+df_columns = np.append(data.feature_names, ['target'])
 
-# Use Support Vector Machines
-svm = SVC()
+data_frame = panda.DataFrame(np_data, columns=df_columns)
+table = data_frame.head()
+print(table)
 
-# Train our data using SVMs
-svm.fit(train, training_labels)
+# We'll use a heatmap to take a better look at our data correlation
+sns.heatmap(data_frame.corr(), annot=True, cmap='coolwarm')
+plt.show()
 
-# Generate predictions
-print(confusion_matrix(test_labels, svm.predict(test)))
-print(accuracy_score(test_labels, svm.predict(test)))
+# Visualization of our data
+# We will visualize a handful of our feature data so everything does not look so fragmented
+sns.pairplot(data_frame, palette='inferno', hue='target', vars=['mean smoothness', 'mean radius', 'mean area'])
+plt.show()
 
-# TBA
+# Feel free to look and compare with all possible features
+# mean radius, mean texture, mean perimeter, mean area, mean smoothness
+# mean compactness, mean concavity, mean concave points, mean symmetry
+# mean fractal dimension, radius error, texture error, perimeter error
+# area error, smoothness error, compactness error, concavity error
+# concave points error, symmetry error, fractal dimension error
+# worst radius, worst texture, worst perimeter, worst area
+# worst smoothness, worst compactness, worst concavity
+# worst concave points, worst symmetry, worst fractal dimension
+
+# Get trained/test data with a test size of 0.5
+x_train, x_test, y_train, y_test = train_test_split(data['data'], data['target'], test_size=0.5, random_state=42)
+
+# Visualize our trained data
+sns.scatterplot(x=x_train[:, 0], y=x_train[:, 1], palette='inferno', hue=y_train, s=10)
+plt.show()
+
+def visualize_confusion_matrix(xt, yt, model):
+    sns.heatmap(confusion_matrix(yt, model.predict(xt)), fmt='d', cmap='RdBu', square=True, annot=True, xticklabels=['Malignant', 'Benign'], yticklabels=['Malignant', 'Benign'])
+    plt.show()
+
+def run_classifiers(x=x_train, y=y_train, xt=x_test, yt=y_test):
+    # Use Naive Bayes
+    model = GaussianNB()
+
+    # Train our data using Naive Bayes
+    model.fit(x, y)
+
+    # Generate predictions
+    print(confusion_matrix(yt, model.predict(xt)))
+    print(accuracy_score(yt, model.predict(xt)))
+    print()
+
+    visualize_confusion_matrix(xt, yt, model)
+
+    # Use Support Vector Machines
+    svm = SVC(kernel='linear')
+
+    # Train our data using SVMs
+    svm.fit(x, y)
+
+    # Generate predictions
+    print(confusion_matrix(yt, svm.predict(xt)))
+    print(accuracy_score(yt, svm.predict(xt)))
+    print()
+
+    visualize_confusion_matrix(xt, yt, svm)
+
+    # Use Logistic Regression
+    log_reg = LogisticRegression()
+
+    # Train our data using Logistic Regression
+    log_reg.fit(x, y)
+
+    # Generate predictions
+    print(confusion_matrix(yt, log_reg.predict(xt)))
+    print(accuracy_score(yt, log_reg.predict(xt)))
+    print()
+
+    visualize_confusion_matrix(xt, yt, log_reg)
+
+# Now we run our classifiers on our trained data
+run_classifiers()
+
+# Now that we have our controls down, let's shuffle our data and see what we get
+np.random.shuffle(x_train)
+np.random.shuffle(x_test)
+np.random.shuffle(y_train)
+np.random.shuffle(y_test)
+
+sns.scatterplot(x=x_train[:, 0], y=x_train[:, 1], palette='inferno', hue=y_train, s=10)
+plt.show()
+
+run_classifiers()
